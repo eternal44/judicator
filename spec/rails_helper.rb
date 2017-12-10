ac4834f51c44e5e3de5b9b8fadca7aa73e588449
James Youn <jamesyoun710@gmail.com>
2017-12-10 00:41:38 -0800

    Configs DB cleaner for specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,4 +56,15 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include RequestSpecHelper, type: :request
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
