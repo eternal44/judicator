fc284e01135305bc5484f65b13233a62080c588d
James Youn <jamesyoun710@gmail.com>
2018-03-11 18:49:43 -0700

    WIP: more clean up

--- app/models/calculators/amortization_schedule.rb
+++ app/models/calculators/amortization_schedule.rb
@@ -34,7 +34,7 @@ class Calculators::AmortizationSchedule < Calculators::CalculationAmount
     generate_schedule([], @starting_principal_cents)
   end
 
-  def generate_schedule(schedule, remaining_balance)
+  def generate_schedule(schedule, remaining_principal)
     period_payment_amount = @scheduled_period_payment_cents
     additional_payment_instructions = @opts[:additional_payments]
 
@@ -44,9 +44,9 @@ class Calculators::AmortizationSchedule < Calculators::CalculationAmount
       period_payment_amount += additional_payment_instructions.fetch(:amount_per_period, 0)
     end
 
-    interest_amount = remaining_balance * @period_interest_rate
+    interest_amount = remaining_principal * @period_interest_rate
     principal_amount = period_payment_amount - interest_amount
-    new_remaining_principal = remaining_balance - principal_amount
+    new_remaining_principal = remaining_principal - principal_amount
 
     period_payment_object = {
       total_payment_for_period: period_payment_amount,
@@ -55,15 +55,13 @@ class Calculators::AmortizationSchedule < Calculators::CalculationAmount
       principal_balance_amount: new_remaining_principal
     }
 
-    if (remaining_balance) <= period_payment_amount
-      final_period_payment_object = {
-        total_payment_for_period: remaining_balance + interest_amount,
-        principal_amount: remaining_balance,
+    if (remaining_principal) <= period_payment_amount
+      schedule << {
+        total_payment_for_period: remaining_principal + interest_amount,
+        principal_amount: remaining_principal,
         interest_amount: interest_amount,
         principal_balance_amount: 0
       }
-
-      schedule << final_period_payment_object
 
       return schedule
     else
--- app/models/calculators/optimal_downpay.rb
+++ app/models/calculators/optimal_downpay.rb
@@ -1,6 +1,4 @@
 class Calculators::OptimalDownpay
-  attr_reader :no_additional_payments_schedule
-
   # NOTE: assume lender will allow bi-monthly payments
   def initialize(schedule_a,
                 schedule_b)
