91dbdc6da3523fd4df2edd24ab5b3e2536729222
James Youn <jamesyoun710@gmail.com>
2018-04-18 00:37:30 -0700

    Refactors compound interest calculator

--- app/models/calculators/compound_interest.rb
+++ app/models/calculators/compound_interest.rb
@@ -1,5 +1,16 @@
 module Calculators::CompoundInterest
+  # TODO: fix rate so it's a whole number
+
+  def self.call(params)
+    total = 0
+    total += continued_contributions(params) unless params.fetch(:periodic_deposit).zero?
+    total += initial_contribution(params) unless params.fetch(:starting_principal).zero?
+
+    total
+  end
+
   def self.initial_contribution(annual_interest_rate:,
+                                periodic_deposit:,
                                 compounding_periods_per_year:,
                                 timeframe_in_years:,
                                 starting_principal:)
@@ -13,7 +24,9 @@ module Calculators::CompoundInterest
   def self.continued_contributions(compounding_periods_per_year:,
                                    annual_interest_rate:,
                                    timeframe_in_years:,
+                                   starting_principal:,
                                    periodic_deposit:)
+
     periodic_interest_rate = annual_interest_rate / compounding_periods_per_year
     number_of_compounding_periods = compounding_periods_per_year * timeframe_in_years
 
