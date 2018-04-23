a22069acbdc815f47455c244f1e92f91ccae88d5
James Youn <jamesyoun710@gmail.com>
2018-04-22 22:02:14 -0700

    WIP: new compounding interest calc

--- app/models/calculators/compound_interest.rb
+++ app/models/calculators/compound_interest.rb
@@ -7,6 +7,8 @@ module Calculators::CompoundInterest
     total
   end
 
+  # NOTE:  referencing this site:
+  #    https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php
   def self.initial_contribution(annual_interest_rate:,
                                 periodic_deposit:,
                                 compounding_periods_per_year:,
@@ -19,6 +21,8 @@ module Calculators::CompoundInterest
       .to_i
   end
 
+  # NOTE: referencing this site:
+  #   https://www.thecalculatorsite.com/articles/finance/compound-interest-formula.php?page=2
   def self.continued_contributions(compounding_periods_per_year:,
                                    annual_interest_rate:,
                                    timeframe_in_years:,
@@ -34,5 +38,22 @@ module Calculators::CompoundInterest
       (1 + periodic_interest_rate)
 
     (total.round(2) * 100).to_i
+  end
+
+  def self.calc(starting_principal:,
+                periodic_deposit:,
+                annual_interest_rate:,
+                compounding_periods_per_year:,
+                timeframe_in_years: )
+
+    # [ P(1+r/n)^(nt) ] + [ PMT × (((1 + r/n)^(nt) - 1) / (r/n)) ]
+
+    periodic_rate = annual_interest_rate.to_f / compounding_periods_per_year
+    compounding_duration = compounding_periods_per_year * timeframe_in_years.to_f
+
+    starting_principal_result = (starting_principal * (1 + periodic_rate) ** compounding_duration)
+    periodic_deposit_result = (periodic_deposit * (((1 + periodic_rate) ** compounding_duration) - 1) / periodic_rate)
+
+    starting_principal_result + periodic_deposit_result
   end
 end
